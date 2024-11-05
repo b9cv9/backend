f7a0f8613afe35b9bd163982d16ebe575ec99aa8
b9cv9 <emil.kurakin95@mail.ru>
2024-11-05 16:40:36 +0400

    Fixed migration, small changes in code

--- src/main.py
+++ src/main.py
@@ -39,7 +39,7 @@ app = create_app()
 
 
 async def run() -> None:
-    config = uvicorn.Config("main:app", host="0.0.0.0", port=8000, reload=False)
+    config = uvicorn.Config("src.main:app", host="0.0.0.0", port=8000, reload=False)
     server = uvicorn.Server(config=config)
     tasks = (
         asyncio.create_task(server.serve()),
--- src/project/infrastructure/postgres/models.py
+++ src/project/infrastructure/postgres/models.py
@@ -12,33 +12,33 @@ class Apartment(Base):
     square_meters: Mapped[float] = mapped_column(nullable=False)
     status_id: Mapped[int] = mapped_column(nullable=False)
     owner_price: Mapped[float] = mapped_column(nullable=False)
-
-    owner_id: Mapped[int] = mapped_column(ForeignKey("owners.id"))
-    owner = relationship("Owner", back_populates="apartments")
-    sales_rent = relationship("SalesRent", back_populates="apartment")
-
-
-class SalesRent(Base):
-    __tablename__ = "sales_rent"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    apartment_id: Mapped[int] = mapped_column(ForeignKey("apartments.id"))
-    renter_id: Mapped[int] = mapped_column(ForeignKey("renters.id"))
-    realtor_id: Mapped[int] = mapped_column(ForeignKey("realtors.id"))
-    sale_date: Mapped[str] = mapped_column(nullable=False)
-    rent_start: Mapped[str] = mapped_column(nullable=True)
-    rent_end: Mapped[str] = mapped_column(nullable=True)
-    sale_price: Mapped[float] = mapped_column(nullable=True)
-
-    apartment = relationship("Apartment", back_populates="sales_rent")
-    renter = relationship("Renter", back_populates="sales_rent")
-
-
-class Owner(Base):
-    __tablename__ = "owners"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    contact_info: Mapped[str] = mapped_column(nullable=True)
-
-    apartments = relationship("Apartment", back_populates="owner")
+#
+#     owner_id: Mapped[int] = mapped_column(ForeignKey("owners.id"))
+#     owner = relationship("Owner", back_populates="apartments")
+#     sales_rent = relationship("SalesRent", back_populates="apartment")
+#
+#
+# class SalesRent(Base):
+#     __tablename__ = "sales_rent"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     apartment_id: Mapped[int] = mapped_column(ForeignKey("apartments.id"))
+#     renter_id: Mapped[int] = mapped_column(ForeignKey("renters.id"))
+#     realtor_id: Mapped[int] = mapped_column(ForeignKey("realtors.id"))
+#     sale_date: Mapped[str] = mapped_column(nullable=False)
+#     rent_start: Mapped[str] = mapped_column(nullable=True)
+#     rent_end: Mapped[str] = mapped_column(nullable=True)
+#     sale_price: Mapped[float] = mapped_column(nullable=True)
+#
+#     apartment = relationship("Apartment", back_populates="sales_rent")
+#     renter = relationship("Renter", back_populates="sales_rent")
+#
+#
+# class Owner(Base):
+#     __tablename__ = "owners"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str] = mapped_column(nullable=False)
+#     contact_info: Mapped[str] = mapped_column(nullable=True)
+#
+#     apartments = relationship("Apartment", back_populates="owner")
